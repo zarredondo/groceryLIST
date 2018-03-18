b540650b2d4b41de90b4fa5c2ea6b09532529b51
zarredondo <zach.arredondo@gmail.com>
2018-03-18 13:27:21 -0500

    Next sequence in the firebase tutorial. Here we retrieve data from the database.

--- app/src/main/java/edu/utexas/ece/pugs/grocerylist/MainActivity.java
+++ app/src/main/java/edu/utexas/ece/pugs/grocerylist/MainActivity.java
@@ -6,61 +6,65 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button mFirebaseBtn;
-
     private DatabaseReference mDatabase;
 
-    private EditText mNameField, mEmailField;
+    private TextView mNameView, mAgeView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mFirebaseBtn = (Button) findViewById(R.id.firebase_btn);
-
         mDatabase = FirebaseDatabase.getInstance().getReference();
+        mNameView = (TextView) findViewById(R.id.name_display);
+        mAgeView = (TextView) findViewById(R.id.age_display);
 
-        mNameField = (EditText) findViewById(R.id.name_field);
-
-        mEmailField = (EditText) findViewById(R.id.email_field);
-
-        mFirebaseBtn.setOnClickListener(new View.OnClickListener() {
+        mDatabase.child("Name").addValueEventListener(new ValueEventListener() {
             @Override
-            public void onClick(View view) {
+            public void onDataChange(DataSnapshot dataSnapshot) {
 
-                String name = mNameField.getText().toString().trim();
-                String email = mEmailField.getText().toString().trim();
+                String name = dataSnapshot.getValue().toString();
 
-                HashMap<String, String> dataMap = new HashMap<>();
+                mNameView.setText("Name: " + name);
 
-                dataMap.put("Name", name);
-                dataMap.put("Email", email);
+            }
 
-                mDatabase.push().setValue(dataMap).addOnCompleteListener(new OnCompleteListener<Void>() {
-                    @Override
-                    public void onComplete(@NonNull Task<Void> task) {
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
 
-                        if (task.isSuccessful()) {
-                            Toast.makeText(MainActivity.this, "Stored successfully", Toast.LENGTH_LONG).show();
-                        } else {
-                            Toast.makeText(MainActivity.this, "Store unsuccessful", Toast.LENGTH_LONG).show();
-                        }
-                    }
-
-                });
             }
         });
+
+        mDatabase.child("Age").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                String age = dataSnapshot.getValue().toString();
+
+                mAgeView.setText("Age: " + age);
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
     }
 }
