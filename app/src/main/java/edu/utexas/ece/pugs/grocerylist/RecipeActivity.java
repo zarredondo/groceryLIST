aacb29156fdaa8fab7df3fb109c7e61995712c72
Brandon <brandonbui50@yahoo.com>
2018-03-20 17:23:44 -0500

    Before Brandon's Initial Commit

---
+++ app/src/main/java/edu/utexas/ece/pugs/grocerylist/RecipeActivity.java
@@ -0,0 +1,8 @@
+package edu.utexas.ece.pugs.grocerylist;
+
+/**
+ * Created by Brandon on 3/16/2018.
+ */
+
+public class RecipeActivity {
+}
---
+++ app/src/main/java/edu/utexas/ece/pugs/grocerylist/RecipeDetailsActivity.java
@@ -0,0 +1,8 @@
+package edu.utexas.ece.pugs.grocerylist;
+
+/**
+ * Created by Brandon on 3/17/2018.
+ */
+
+public class RecipeDetailsActivity {
+}
