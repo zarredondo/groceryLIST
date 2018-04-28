89d3a050b96428d95ce64639ea469b241266eaf0
Manuel Lopez <manuellopez@Manuels-MacBook-Pro.local>
2018-04-28 11:54:37 -0500

    added Qty and weight

--- app/src/main/java/edu/utexas/ece/pugs/grocerylist/PantryActivity.java
+++ app/src/main/java/edu/utexas/ece/pugs/grocerylist/PantryActivity.java
@@ -5,8 +5,10 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AutoCompleteTextView;
 import android.widget.TextView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
@@ -152,15 +154,16 @@ public class PantryActivity extends AppCompatActivity {
         switch(item.getItemId()){
             case R.id.action_add_item:
                 final EditText itemEditText = new EditText(this);
+                //itemEditText.setHint("Qty, Weight, product");
                 AlertDialog dialog = new AlertDialog.Builder(this)
-                        .setTitle("Add New Item")
-                        .setMessage("What's next")
+                        .setTitle("Add New Pantry Item")
+                        .setMessage("Qty Weight Item")
                         .setView(itemEditText)
                         .setPositiveButton("Add", new DialogInterface.OnClickListener(){
                             @Override
                             public void onClick(DialogInterface dialog, int chich){
                                 final String item = String.valueOf(itemEditText.getText());
-                                controller.createParseIngredientsAsync(item, 1, new APICallBack<DynamicResponse>() {
+                                controller.createParseIngredientsAsync(item, 2, new APICallBack<DynamicResponse>() {
                                     @Override
                                     public void onSuccess(HttpContext context, DynamicResponse response) {
                                         try {
@@ -172,6 +175,7 @@ public class PantryActivity extends AppCompatActivity {
                                         Purchase pur = new Purchase();
                                         Quantity quan = new Quantity();
                                         for(Map.Entry<String, Object> entry: result.get(0).entrySet()) {
+                                            System.out.println(entry.getValue().toString());
                                             if(entry.getKey() == "id") {
                                                 pur.setId(entry.getValue().toString());
                                             }
@@ -180,7 +184,7 @@ public class PantryActivity extends AppCompatActivity {
                                             }else if(entry.getKey() == "name") {
                                                 pur.setName(entry.getValue().toString());
                                             }else if(entry.getKey() == "amount") {
-                                                quan.setAmount(1);
+                                                quan.setAmount((int) Double.parseDouble(entry.getValue().toString()));
                                             }else if(entry.getKey() == "consistency") {
                                                 pur.setConsistency(entry.getValue().toString());
                                             }else if(entry.getKey() == "aisle") {
