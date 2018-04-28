88b27162455795ff61e2aaeac6081316756c1f96
zarredondo <zach.arredondo@gmail.com>
2018-04-28 11:28:08 -0500

    Added some test classes. Not working still.

---
+++ app/src/main/java/edu/utexas/ece/pugs/grocerylist/Settings.java
@@ -0,0 +1,8 @@
+package edu.utexas.ece.pugs.grocerylist;
+
+/**
+ * Created by zarredondo on 4/25/2018.
+ */
+
+public class Settings {
+}
---
+++ app/src/test/java/edu/utexas/ece/pugs/grocerylist/foodstuff/PantryTest.java
@@ -0,0 +1,8 @@
+import static org.junit.Assert.*;
+
+/**
+ * Created by zarredondo on 4/25/2018.
+ */
+public class PantryTest {
+
+}
